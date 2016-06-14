e70b9bd9b2890809bfba821096c833831beeac5d
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-06-14 15:26:29 +0100

    now with working links to table pages

--- beta.py
+++ beta.py
@@ -1,4 +1,5 @@
 # http://127.0.0.1:5000/
+import numpy as np
 from load_tables import load_tables
 from arxiv import get_article
 from flask import Flask, request
@@ -16,18 +17,24 @@ def index():
 @app.route('/find-tables', methods=["POST"])
 def scrape():
     arxiv_number = str(request.form["arxiv_number"])
+    with open("number.txt", "w") as f:
+        f.write("{0}".format(arxiv_number))
     get_article(arxiv_number)
     data_list, header_list, unit_list = load_tables(arxiv_number)
     return render_template('data.html', ntables=len(data_list),
                            header="{0}".format(header_list[0]),
                            data="{0}".format(data_list[0]))
 
-# # Display table and select variables
-# @app.route('/table/<int:tnumber>', methods=["POST"])
-# def select_variables(tnumber):
-#     data_list, header_list, unit_list = load_tables(arxiv_number)
-#     return render_template('table.html', header="{0}".format(header_list[0]),
-#                            data="{0}".format(data_list[0]))
+
+# Display table and select variables
+@app.route('/table/<int:tnumber>', methods=["GET", "POST"])
+def select_variables(tnumber):
+    with open("number.txt", "r") as f:
+        arxiv_number = f.read()
+    data_list, header_list, unit_list = load_tables(arxiv_number)
+    return render_template('table.html', ntables=len(data_list),
+                           header="{0}".format(header_list[0]),
+                           data="{0}".format(data_list[0]))
 
 
 if __name__ == '__main__':
