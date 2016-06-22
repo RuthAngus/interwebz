332cd337b15049b3f5175d364fcfbaa9b7dc0e00
mjd12010 <meredith.durbin@gmail.com>
2016-06-22 17:33:14 +0100

    Change axis label font size

--- bokeh_plot.py
+++ bokeh_plot.py
@@ -11,9 +11,19 @@ from bokeh.plotting import Figure, output_file, save
 output_file("callback.html") # currently writes to a file
 # should change to output JS and HTML strings to pass to template
 
+def get_error_tuples(val,err,pos,alpha=0.6):
+	# val: the coordinates on the axis with error bars (i.e., y coordinates if plotting y errs, etc)
+	# err: the given error (assuming 1 sigma)
+	# pos: the coordinates on the axis opposite the error bars (x coordinates if y errs, etc)
+	err_width = [(i, j) for i, j in zip(x - xerr, x + xerr)]
+	err_pos = [(i, i) for i in pos]
+	#plot.multi_line(err_width, err_ypos, alpha=alpha)
+
 def do_a_plot(table):
 	table['blank_x'] = '' # add fake columns for plotting
 	table['blank_y'] = ''
+	table['blank_x_err'] = ''
+	table['blank_y_err'] = ''
 	source = ColumnDataSource(data=dict(table))
 
 	plot = Figure(plot_width=500, plot_height=500)
@@ -21,7 +31,7 @@ def do_a_plot(table):
 	# line = plot.line('blank_x', 'blank_y', source=source, visible=False, _changing=True)
 
 	main_callback = CustomJS(args=dict(source=source,
-		xaxis=plot.xaxis[0],#[0].axis_label,
+		xaxis=plot.xaxis[0],
 		yaxis=plot.yaxis[0]), code="""
 	        var data = source.get('data');
 	        var f = cb_obj.get('value');
@@ -53,6 +63,9 @@ def do_a_plot(table):
 	select_y = Select(title="Y Options:", value=column_list[0], options=column_list, callback=main_callback)
 	reverse_x_button = Button(label="Reverse X range", type="success", callback=reverse_x_callback)
 	reverse_y_button = Button(label="Reverse Y range", type="success", callback=reverse_y_callback)
+
+	plot.xaxis.axis_label_text_font_size = '14pt'
+    plot.yaxis.axis_label_text_font_size = '14pt'
 
 	layout = vform(select_x, select_y, reverse_x_button, reverse_y_button, plot)
 
